5e56496fdaca0bbdf678bbd879755bf8e1101dd4
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-06-01 00:09:54 +1000

    Use vuetify breakpoint code for carousel navigation check

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -3,7 +3,7 @@
     :slides-per-view="panelViewItemResponsive($vuetify.display.width) + 0.5"
     :space-between="24"
     :free-mode="true"
-    :navigation="getBreakpointValue({ breakpoint: 'mobile' }) ? false : true"
+    :navigation="mobile"
   >
     <slot></slot>
   </swiper>
@@ -11,8 +11,11 @@
 
 <script setup lang="ts">
 import { panelViewItemResponsive } from '@/helpers/utils';
-import { getBreakpointValue } from '@/plugins/breakpoint';
 import { onMounted } from 'vue';
+import { useDisplay } from 'vuetify';
+
+// TODO: Remove `mobileBreakpoint` customisation once we have removed `getBreakpointValue` and use a consistent mobile value everywhere.
+const { mobile } = useDisplay({ mobileBreakpoint: 575 });
 
 onMounted(() => {
   document.documentElement.style.setProperty(
